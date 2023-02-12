2af73a72f72afe6e129990b050c0b6ca9ff5f1a6
锈渎 <844506672@qq.com>
2023-02-12 12:48:10 +0800

    创建导航栏

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,34 +1,16 @@
 <template>
-  <div style="color:white">
-    {{ name1 }}
-    {{ name2 }}
-  </div>
+  <NavBar/>
   <router-view/>
 </template>
 
 <script>
-import { ref } from 'vue';
-import $ from "jquery";
-
+import NavBar from "@/components/NavBar.vue"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap"
 export default{
-  
-  setup: () => {
-    let name1 = ref("");
-    let name2 = ref("");
-    $.ajax({
-      url: "http://127.0.0.1:3000/home/",
-      type: "get",
-      success(resp){
-        name1.value = resp.name1;
-        name2.value = resp.name2;
-      }
-    })
-    return {
-      name1, 
-      name2
+    components: {
+      NavBar
     }
-  },
-  
 }
 </script>
 <style>
--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,9 +1,50 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import HomeIndexView from "@/views/home/HomeIndexView"
+import PkIndexView from "@/views/pk/PkIndexView"
+import RanklistIndexView from "@/views/ranklist/RanklistIndexView"
+import RecordIndexView from "@/views/record/RecordIndexView"
+import UserIndexView from "@/views/user/UserIndexView"
+import NotFound from "@/views/error/NotFound"
 
 const routes = [
   {
+      path: "/",
+      name: "home",
+      redirect: "/home/",
   },
   {
+      path: "/home/",
+      name: "home_index",
+      component: HomeIndexView,
+  },
+  {
+      path: "/pk/",
+      name: "pk_index",
+      component: PkIndexView,
+  },
+  {
+      path: "/ranklist/",
+      name: "ranklist_index",
+      component: RanklistIndexView,
+  },
+  {
+      path: "/record/",
+      name: "record_index",
+      component: RecordIndexView,
+  },
+  {
+      path: "/user/",
+      name: "user_index",
+      component: UserIndexView,
+  },
+  {
+      path: "/404/",
+      name: "404",
+      component: NotFound,
+  },
+  {
+      path: "/:catAll(.*)",
+      redirect: "/404/",
   }
 ]
 
